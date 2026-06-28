bca3756cfb5c444c68fa5e8937a37ffe747d60e9
nedarctic <nedarctic@gmail.com>
2026-06-28 14:33:47 +0300

    display + pagiation + search bookings general page

--- app/(dashboard)/bookings/page.tsx
+++ app/(dashboard)/bookings/page.tsx
@@ -1,9 +1,33 @@
 import { BreadCrumb } from "@/components/breadcrumb";
+import { TableData } from "@/components/table-data";
+import { SearchInput } from "@/components/search-input";
+import { PaginationComponent } from "@/components/pagination";
+import { getBookings } from "@/lib/helpers/bookings.helpers";
 
-export default async function BookingsPage () {
+export default async function BookingsPage(
+    { searchParams }: {
+        searchParams: Promise<{
+            page?: string;
+            limit?: string;
+            search?: string;
+        }>
+    }
+) {
+
+    const { limit, page, search } = await searchParams;
+    const { success, data, error } = await getBookings({ page, limit, search });
+
+    const {bookings, meta} = data;
+
+    error && console.log('An error occurred fetching bookings', error);
+    success && console.log('Successfully fetched bookings', bookings);
+
     return (
-        <div className="flex flex-col py-6">
-              <BreadCrumb currentPage="Bookings" />
+        <div className="flex flex-col py-6 ml-4 mr-6 gap-6">
+            <BreadCrumb currentPage="Bookings" />
+            <SearchInput />
+            <TableData bookingData={bookings} />
+            <PaginationComponent meta={meta} />
         </div>
-    );    
+    );
 }

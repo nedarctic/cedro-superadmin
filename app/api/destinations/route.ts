888bac5d6f034aa3f70e0248ab91d4e109927376
nedarctic <nedarctic@gmail.com>
2026-06-29 16:33:58 +0300

    destination post api route handler

---
+++ app/api/destinations/route.ts
@@ -0,0 +1,47 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+export async function POST(req: NextRequest) {
+    try {
+        const session = await getServerSession(authOptions);
+        if (!session) {
+            redirect('/login');
+        }
+
+        const { accessToken } = session;
+
+        const body = await req.json();
+
+        const res = await fetch(`${process.env.NEST_API_URL}/destinations`, {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        });
+
+        if (!res.ok) {
+            const errorMessage = await res.json();
+            return NextResponse.json({
+                success: false,
+                error: errorMessage.message,
+            })
+        }
+
+        const { data, success } = await res.json();
+
+        return NextResponse.json({
+            success,
+            data
+        })
+
+    } catch (error) {
+        return NextResponse.json({
+            success: false,
+            error: error instanceof Error ? error.message : String(error)
+        }, { status: 500 })
+    }
+}

35d12611594cb8ab581b63808a99e325f771a405
nedarctic <nedarctic@gmail.com>
2026-06-29 16:26:36 +0300

    add total bookings field for tour type

--- lib/types/tour.ts
+++ lib/types/tour.ts
@@ -3,6 +3,7 @@ import { Booking } from "./booking";
 export type Tour = {
     id: string;
     title: string;
+    totalBookings?: string;
     bookings?: Booking[];
     createdAt: string;
     updatedAt: string;

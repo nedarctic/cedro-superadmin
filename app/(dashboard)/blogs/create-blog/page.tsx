f79ac373317428d40eb3d8f8386995a55f1ee7e0
nedarctic <nedarctic@gmail.com>
2026-07-01 10:37:46 +0300

    create blog page

---
+++ app/(dashboard)/blogs/create-blog/page.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { BreadCrumb } from "@/components/breadcrumb";
+import { CreateBlogForm } from "@/components/create-blog-form";
+
+export default function CreateBlogPage() {
+    
+    const crumbs = [
+        {label: "Blogs", link: '/blogs'},
+    ];
+
+    return (
+        <div className="flex flex-col py-6 ml-4 mr-6 gap-6">
+            <BreadCrumb crumbs={crumbs} currentPage="Create Blog" />
+
+            <h1 className="font-bold text-xl">Blog Creation Form</h1>
+
+            <CreateBlogForm />
+        </div>
+    );
+}

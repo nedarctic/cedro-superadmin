7b14ace96c86375ba5490a611a396de4ff3e42c0
nedarctic <nedarctic@gmail.com>
2026-07-01 10:36:55 +0300

    add create tour link

--- app/(dashboard)/blogs/page.tsx
+++ app/(dashboard)/blogs/page.tsx
@@ -1,9 +1,14 @@
 import { BreadCrumb } from "@/components/breadcrumb";
+import { PlusIcon } from "lucide-react";
+import Link from "next/link";
 
-export default async function BlogsPage () {
+export default async function BlogsPage() {
     return (
-        <div className="flex flex-col py-6">
-              <BreadCrumb currentPage="Blogs" />
+        <div className="flex flex-col py-6 ml-4 mr-6 gap-6">
+            <div className="flex justify-between">
+                <BreadCrumb currentPage="Blogs" />
+                <Link className="flex gap-2 py-1 px-2 border-black text-sm bg-black rounded-md text-white items-center" href="/blogs/create-blog">Create new blog <PlusIcon size={16} /> </Link>
+            </div>
         </div>
     )
 }

af04bc205914015c4006d61cefe81d18f8c38cb0
nedarctic <nedarctic@gmail.com>
2026-07-02 01:16:10 +0300

    create tour page link

--- app/(dashboard)/team/page.tsx
+++ app/(dashboard)/team/page.tsx
@@ -1,9 +1,14 @@
 import { BreadCrumb } from "@/components/breadcrumb";
+import { PlusIcon } from "lucide-react";
+import Link from "next/link";
 
-export default async function TeamPage () {
+export default async function TeamPage() {
     return (
-        <div className="flex flex-col py-6">
-              <BreadCrumb currentPage="Team" />
+        <div className="flex flex-col py-6 ml-4 mr-6 gap-6">
+            <div className="flex justify-between">
+                <BreadCrumb currentPage="Team" />
+                <Link className="flex gap-2 py-1 px-2 border-black text-sm bg-black rounded-md text-white items-center" href="/team/create-member">Add new member <PlusIcon size={16} /> </Link>
+            </div>
         </div>
     )
 }

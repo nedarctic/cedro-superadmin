c43cd0c613117f44356fc59a3a5825035ab9ec8d
nedarctic <nedarctic@gmail.com>
2026-07-02 19:07:45 +0300

    delimit team member description field

--- components/create-member-form.tsx
+++ components/create-member-form.tsx
@@ -2,7 +2,7 @@
 
 import { useState, SubmitEvent } from 'react';
 import { Form } from './ui/form';
-import { Field } from './ui/field';
+import { Field, FieldLabel } from './ui/field';
 import { Input } from './ui/input';
 import { Button } from './ui/button';
 import z from 'zod';
@@ -10,11 +10,12 @@ import { useRouter } from 'next/navigation';
 import { Dialog, DialogContent } from './ui/dialog';
 import { CustomSpinner } from './custom-spinner';
 import { toast } from 'sonner';
+import { Textarea } from './ui/textarea';
 
 const memberCreationSchema = z.object({
     name: z.string().min(1, { message: "Name is required" }),
     designation: z.string().min(1, { message: "Designation is required" }),
-    description: z.string().min(1, { message: "Description is required" }),
+    description: z.string().min(1, { message: "Description is required" }).max(200, "Description should not exceed 200 characters"),
     memberImage: z.instanceof(File, { message: "Member image is required" })
         .refine(file => file.size > 0, { message: "File is required" })
         .refine(file => file.size <= 5 * 1024 * 1024, { message: "File size must be less than 5MB" })
@@ -89,7 +90,9 @@ export function CreateMemberForm() {
         <Form className="flex flex-col w-full max-w-7xl mx-auto gap-4" onSubmit={handleSubmit}>
 
             <Field>
+                <FieldLabel htmlFor='name'>Name</FieldLabel>
                 <Input placeholder="Name"
+                    id="name"
                     value={name}
                     onChange={(e) => setName(e.target.value)}
                 />
@@ -97,24 +100,30 @@ export function CreateMemberForm() {
             </Field>
 
             <Field>
+                <FieldLabel htmlFor='designation'>Designation</FieldLabel>
                 <Input placeholder="Designation"
+                    id='designation'
                     value={designation}
                     onChange={(e) => setDesignation(e.target.value)} />
-                    {errors?.properties?.designation?.errors?.length && <ul className="list-disc pl-4">{errors.properties.designation.errors.map((error: string, index: number) => (<li className="font-bold text-[12px] text-red-600" key={index}>{error}</li>))}</ul>}
+                {errors?.properties?.designation?.errors?.length && <ul className="list-disc pl-4">{errors.properties.designation.errors.map((error: string, index: number) => (<li className="font-bold text-[12px] text-red-600" key={index}>{error}</li>))}</ul>}
             </Field>
 
             <Field>
-                <Input placeholder="Description"
+                <FieldLabel htmlFor='description'>Description</FieldLabel>
+                <Textarea placeholder="Description"
+                    id='description'
                     value={description}
                     onChange={(e) => setDescription(e.target.value)} />
-                    {errors?.properties?.description?.errors?.length && <ul className="list-disc pl-4">{errors.properties.description.errors.map((error: string, index: number) => (<li className="font-bold text-[12px] text-red-600" key={index}>{error}</li>))}</ul>}
+                {errors?.properties?.description?.errors?.length && <ul className="list-disc pl-4">{errors.properties.description.errors.map((error: string, index: number) => (<li className="font-bold text-[12px] text-red-600" key={index}>{error}</li>))}</ul>}
             </Field>
 
             <Field>
+                <FieldLabel htmlFor='memberImage'>Select member image</FieldLabel>
                 <Input type="file"
+                    id='memberImage'
                     accept='image/png, image/jpeg'
                     onChange={(e) => setMemberImage(e.target.files ? e.target.files[0] : null)} />
-                    {errors?.properties?.memberImage?.errors?.length && <ul className="list-disc pl-4">{errors.properties.memberImage.errors.map((error: string, index: number) => (<li className="font-bold text-[12px] text-red-600" key={index}>{error}</li>))}</ul>}
+                {errors?.properties?.memberImage?.errors?.length && <ul className="list-disc pl-4">{errors.properties.memberImage.errors.map((error: string, index: number) => (<li className="font-bold text-[12px] text-red-600" key={index}>{error}</li>))}</ul>}
             </Field>
 
             <Button type="submit"

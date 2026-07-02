a440c1ad5e50cdaa5588c6f1fac72a25ea725379
nedarctic <nedarctic@gmail.com>
2026-07-02 19:04:22 +0300

    team member type

---
+++ lib/types/team-member.ts
@@ -0,0 +1,10 @@
+export type TeamMember = {
+    id: string;
+    name: string;
+    designation: string;
+    description: string;
+    memberImageKey: string;
+    memberImageUrl: string;
+    createdAt: string;
+    updatedAt: string;
+}

b3beb63425b21418e443c9ca0deab42d85955b1c
nedarctic <nedarctic@gmail.com>
2026-07-02 19:11:43 +0300

    image server configured

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pub-5cee8e6d1a574b6c84697dfdb9beba4a.r2.dev"
+      }
+    ]
+  }
 };
 
 export default nextConfig;

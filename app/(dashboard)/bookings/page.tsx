45593a422290a6d88af2af1740e8ab36d0c7ccac
nedarctic <nedarctic@gmail.com>
2026-07-02 19:10:32 +0300

    height adjustment and path prop strip

--- app/(dashboard)/bookings/page.tsx
+++ app/(dashboard)/bookings/page.tsx
@@ -3,6 +3,8 @@ import { TableData } from "@/components/table-data";
 import { SearchInput } from "@/components/search-input";
 import { PaginationComponent } from "@/components/pagination";
 import { getBookings } from "@/lib/helpers/bookings.helpers";
+import Link from "next/link";
+import { PlusIcon } from "lucide-react";
 
 export default async function BookingsPage(
     { searchParams }: {
@@ -14,7 +16,7 @@ export default async function BookingsPage(
     }
 ) {
 
-    const { limit, page, search } = await searchParams;
+    const { limit = "10", page, search } = await searchParams;
     const { success, data, error } = await getBookings({ page, limit, search });
 
     const { bookings, meta } = data;
@@ -29,11 +31,16 @@ export default async function BookingsPage(
     ];
 
     return (
-        <div className="flex flex-col py-6 ml-4 mr-6 gap-6">
-            <BreadCrumb currentPage="Bookings" />
-            <SearchInput path="bookings" placeholder="Search bookings..." />
-            {bookings.length ? <TableData headers={headers} data={bookings} /> : <p className="text-sm font-medium">No bookings at the moment</p>}
-            {bookings.length ? <PaginationComponent path="bookings" meta={meta} /> : ''}
+        <div className="flex flex-col py-6 ml-4 mr-6 gap-6 h-full">
+            <div className="flex justify-between">
+                <BreadCrumb currentPage="Bookings" />
+                <Link className="flex gap-2 py-1 px-2 border-black text-sm bg-black rounded-md text-white items-center" href="/bookings/create-booking"><PlusIcon size={16} />Create new booking</Link>
+            </div>
+            <SearchInput placeholder="Search bookings..." />
+            <div className="flex flex-col justify-between min-h-4/5">
+                {bookings.length ? <TableData headers={headers} data={bookings} /> : <p className="text-sm font-medium">No bookings at the moment</p>}
+                {bookings.length ? <PaginationComponent meta={meta} /> : ''}
+            </div>
         </div>
     );
 }

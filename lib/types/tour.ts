7aa1d0eafec94ce28a8132dc5d828fbffbfd9f0e
nedarctic <nedarctic@gmail.com>
2026-07-02 19:04:04 +0300

    added more fields to type

--- lib/types/tour.ts
+++ lib/types/tour.ts
@@ -1,8 +1,23 @@
 import { Booking } from "./booking";
+import { Destination } from "./destination";
+import { Itinerary } from "./itinerary";
 
 export type Tour = {
     id: string;
     title: string;
+    description: string[];
+    activities: string[];
+    included: string[];
+    excluded: string[];
+    destination: Destination;
+    dates: string;
+    duration: string;
+    tourImageKey: string;
+    tourImageUrl: string;
+    groupSize: string;
+    price: string;
+    itineraries: Itinerary[];
+    destinationId: string;
     totalBookings?: string;
     bookings?: Booking[];
     createdAt: string;

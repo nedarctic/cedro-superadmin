c5fdc9538e365c7e985a856eb14d452b0cf50b95
nedarctic <nedarctic@gmail.com>
2026-07-03 09:46:01 +0300

    added get team member helper

--- lib/helpers/team.helpers.ts
+++ lib/helpers/team.helpers.ts
@@ -1,5 +1,6 @@
 import { TeamMember } from "../types/team-member";
 
+// get team
 export async function getTeam (options: {
     page?: string;
     limit?: string;
@@ -45,6 +46,35 @@ export async function getTeam (options: {
             data
         }
 
+    } catch (error) {
+        return {
+            success: false,
+            error: error instanceof Error ? error.message : String(error)
+        }
+    }
+}
+
+// get team member
+export async function getMember (memberId: string) {
+    try {
+        const res = await fetch(`${process.env.NEST_API_URL}/team/${memberId}`, {
+            method: 'GET'
+        });
+
+        if (!res.ok) {
+            const error = (await res.json()).error.message;
+            return {
+                success: false,
+                error: error || 'Backend request error'
+            }
+        }
+
+        const { success, data } = await res.json();
+        
+        return {
+            success,
+            data
+        }
     } catch (error) {
         return {
             success: false,

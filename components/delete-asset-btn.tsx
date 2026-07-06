d5575df9bcca4c2d7e9b488e2b131f488d00e9a1
nedarctic <nedarctic@gmail.com>
2026-07-06 23:20:51 +0300

    dialog for delete asset

--- components/delete-asset-btn.tsx
+++ components/delete-asset-btn.tsx
@@ -1,36 +1,36 @@
 import { Button } from "@/components/ui/button";
 import {
-    Dialog,
-    DialogClose,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog";
 import { TrashIcon } from "lucide-react";
 
-export function DeleteAssetBtn ({label, deleteHandler}: {label: string; deleteHandler?: () => void}) {
+export function DeleteAssetBtn({ label, deleteHandler }: { label: string; deleteHandler?: () => void }) {
   return (
     <Dialog>
-        <DialogTrigger asChild>
-            <Button variant="destructive"><TrashIcon size={16}/>{label}</Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-sm">
-          <DialogHeader>
-            <DialogTitle>{label}</DialogTitle>
-            <DialogDescription>
-              Are you sure you want to delete? This action is irreversible.
-            </DialogDescription>
-          </DialogHeader>
-          <DialogFooter>
+      <DialogTrigger asChild>
+        <Button variant="destructive"><TrashIcon size={16} />{label}</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-sm">
+        <DialogHeader>
+          <DialogTitle>{label}</DialogTitle>
+          <DialogDescription>
+            Are you sure you want to delete? This action is irreversible.
+          </DialogDescription>
+          <div className="flex flex-row justify-end gap-2">
             <DialogClose asChild>
-              <Button variant="outline">Cancel</Button>
+              <Button className="max-w-fit" variant="outline">Cancel</Button>
             </DialogClose>
-            <Button variant="destructive" type="submit">Delete</Button>
-          </DialogFooter>
-        </DialogContent>
+            <Button className="max-w-fit" variant="destructive" type="submit">Delete</Button>
+          </div>
+        </DialogHeader>
+      </DialogContent>
     </Dialog>
   )
 }

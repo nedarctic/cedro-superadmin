52f3a91360f01639f8ee1b49b0da16486d0ab32c
nedarctic <nedarctic@gmail.com>
2026-07-09 23:13:11 +0300

    delete tour buttton swap

--- app/(dashboard)/tours/[tourId]/page.tsx
+++ app/(dashboard)/tours/[tourId]/page.tsx
@@ -1,9 +1,7 @@
-import { BreadCrumb } from "@/components/breadcrumb"
-import { Button } from "@/components/ui/button"
-import { PencilIcon } from "lucide-react"
-import { getTour } from "@/lib/helpers/tours.helpers";
+import { BreadCrumb } from "@/components/breadcrumb";
+import { DeleteTourBtn } from "@/components/delete-tour-btn";
 import { EditAssetBtn } from "@/components/edit-asset-btn";
-import { DeleteAssetBtn } from "@/components/delete-asset-btn";
+import { getTour } from "@/lib/helpers/tours.helpers";
 import { notFound } from "next/navigation";
 
 export default async function TourDetailsPage({ params }: { params: Promise<{ tourId: string }> }) {
@@ -11,7 +9,7 @@ export default async function TourDetailsPage({ params }: { params: Promise<{ to
     const { tourId } = await params;
     const { success, data, error } = await getTour(tourId)
 
-    if(!success){
+    if (!success) {
         return notFound();
     }
 
@@ -24,8 +22,8 @@ export default async function TourDetailsPage({ params }: { params: Promise<{ to
             <div className="flex justify-between">
                 <BreadCrumb crumbs={crumbs} currentPage="Tour Details" />
                 <div className="flex items-center gap-4">
-                    <EditAssetBtn path={`/tours/${tourId}/edit-tour`} label={"Edit Tour"}/>
-                    <DeleteAssetBtn label="Delete Tour" />
+                    <EditAssetBtn path={`/tours/${tourId}/edit-tour`} label={"Edit Tour"} />
+                    <DeleteTourBtn destinationId={data?.destinationId!} tourId={tourId} />
                 </div>
             </div>
             <div className="min-h-4/5 flex flex-col items-start justify-start">

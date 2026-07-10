dd6a4ba6d36330734599b3345d44ac96e1b04877
nedarctic <nedarctic@gmail.com>
2026-07-10 13:04:01 +0300

    booking details page

--- app/(dashboard)/bookings/[bookingId]/page.tsx
+++ app/(dashboard)/bookings/[bookingId]/page.tsx
@@ -1,33 +1,45 @@
-import { BreadCrumb } from "@/components/breadcrumb"
-import { Button } from "@/components/ui/button"
-import { PencilIcon } from "lucide-react"
-import { getBooking } from "@/lib/helpers/bookings.helpers";
+import { BreadCrumb } from "@/components/breadcrumb";
+import { DeleteBookingBtn } from "@/components/delete-booking-btn";
 import { EditAssetBtn } from "@/components/edit-asset-btn";
-import { DeleteAssetBtn } from "@/components/delete-asset-btn";
+import { UpdateBookingDrawer } from "@/components/update-booking-drawer";
+import { getBooking } from "@/lib/helpers/bookings.helpers";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 
 export default async function BookingDetailsPage({ params }: { params: Promise<{ bookingId: string }> }) {
 
     const { bookingId } = await params;
     const { success, data, error } = await getBooking(bookingId)
 
-    !success && console.log('An error occurred fetching booking', error);
-    success && console.log('Successfully fetched booking data', data);
-
+    !success && notFound();
     const crumbs = [
         { label: 'Bookings', link: '/bookings' },
     ]
 
     return (
-        <div className="flex flex-col py-6 ml-4 mr-6 gap-6 h-full">
+        <div className="flex flex-col py-6 ml-4 mr-6 gap-6 min-h-screen">
             <div className="flex justify-between">
                 <BreadCrumb crumbs={crumbs} currentPage="Booking Details" />
                 <div className="flex items-center gap-4">
-                    <EditAssetBtn label="Edit Booking" path={`/bookings/${bookingId}/edit-booking`} />
-                    <DeleteAssetBtn label="Delete Booking" />
+                    <UpdateBookingDrawer booking={data!} />
+                    <DeleteBookingBtn
+                        bookingId={bookingId}
+                        destinationId={data?.tour?.destinationId!}
+                        tourId={data?.tourId!}
+                    />
                 </div>
             </div>
-            <div className="min-h-4/5 flex flex-col items-start justify-start">
-                <h1 className="font-bold text-xl">{data?.tourTitle}</h1>
+            <div className="h-full w-full flex flex-col items-start justify-start space-y-6">
+                <h1 className="font-bold text-xl">{data?.name}&apos;s {data?.tour.title} Booking Details</h1>
+                <div className="flex flex-col gap-2 w-full h-full">
+                    <div className="relative aspect-video w-full max-w-7xl ">
+                        <Image src={data?.tour?.tourImageUrl!} alt={"Tour image"} fill unoptimized className="rounded-2xl" />
+                    </div>
+                    <p><span className="font-bold">Name: </span>{data?.name}</p>
+                    <p><span className="font-bold">Email: </span>{data?.email}</p>
+                    <p><span className="font-bold">Tour: </span>{data?.tour?.title}</p>
+                    <p><span className="font-bold">Destination: </span>{data?.tour?.destination?.name}</p>
+                </div>
             </div>
         </div>
     )

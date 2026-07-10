f0dd3a937967e29d0092bd71521fb4918c7797ab
nedarctic <nedarctic@gmail.com>
2026-07-10 16:30:55 +0300

    get logged in user

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,8 +2,28 @@ import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import React from "react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
 export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+        redirect('/login');
+    };
+
+    const { user } = session;
+    const initials = user.name.split(' ').map(name => name[0]).reduce((prev, curr) => {
+        return prev + curr
+    }, '');
+
+    const loggedInUser = {
+        name: user.name,
+        email: user.email,
+        avatar: initials
+    };
+
     return (
         <TooltipProvider>
             <SidebarProvider
@@ -14,7 +34,7 @@ export default async function DashboardLayout({ children }: { children: React.Re
                     } as React.CSSProperties
                 }
             >
-                <AppSidebar />
+                <AppSidebar user={loggedInUser} />
                 <div className="w-full my-2">
                     {children}
                 </div>

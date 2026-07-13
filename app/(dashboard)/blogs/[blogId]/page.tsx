5e8aee3739b49bdabfa270940d3749a78bdf0517
nedarctic <nedarctic@gmail.com>
2026-07-13 09:14:54 +0300

    blog details page

--- app/(dashboard)/blogs/[blogId]/page.tsx
+++ app/(dashboard)/blogs/[blogId]/page.tsx
@@ -1,9 +1,8 @@
-import { BreadCrumb } from "@/components/breadcrumb"
-import { Button } from "@/components/ui/button"
-import { PencilIcon } from "lucide-react"
-import { getBlog } from "@/lib/helpers/blogs.helpers";
+import { BreadCrumb } from "@/components/breadcrumb";
+import { DeleteBlogBtn } from "@/components/delete-blog-btn";
 import { EditAssetBtn } from "@/components/edit-asset-btn";
-import { DeleteAssetBtn } from "@/components/delete-asset-btn";
+import { getBlog } from "@/lib/helpers/blogs.helpers";
+import Image from "next/image";
 
 export default async function BlogDetailsPage({ params }: { params: Promise<{ blogId: string }> }) {
 
@@ -23,11 +22,45 @@ export default async function BlogDetailsPage({ params }: { params: Promise<{ bl
                 <BreadCrumb crumbs={crumbs} currentPage="Blog Details" />
                 <div className="flex items-center gap-4">
                     <EditAssetBtn label="Edit Blog" path={`/blogs/${blogId}/edit-blog`} />
-                    <DeleteAssetBtn label={"Delete Blog"} />
+                    <DeleteBlogBtn blogId={blogId} />
                 </div>
             </div>
-            <div className="min-h-4/5 flex flex-col items-start justify-start">
+            <div className="min-h-4/5 flex flex-col items-start justify-start gap-6">
                 <h1 className="font-bold text-xl">{data?.title}</h1>
+
+                <div className="relative aspect-video w-full max-w-7xl">
+                    <Image src={data?.blogImageUrl!} alt={"Blog image"} fill unoptimized className="rounded-2xl" />
+                </div>
+
+                <div className="flex flex-col gap-4 w-full">
+
+                    <div className="flex flex-col gap-2">
+                        <h2 className="text-lg font-extrabold">Introduction</h2>
+                        <p className="text-sm">{data?.intro}</p>
+                    </div>
+
+                    <div className="flex flex-col gap-2 w-full">
+                        <ul className="flex flex-col list-none gap-10">
+                            {data?.sections?.map
+                                ((section, index) =>
+                                    <li key={index} className="flex flex-col gap-2">
+                                        <p className="font-semibold text-md underline">{section.subtitle}</p>
+                                        <p className="text-sm">{section.content}</p>
+                                        {section.sectionImageUrl && <div className="relative aspect-video w-full max-w-7xl">
+                                            <Image src={section.sectionImageUrl} alt={`Blog ${section.section} image`} fill className="rounded-xl" unoptimized />
+                                        </div>}
+                                    </li>
+                                )
+                            }
+                        </ul>
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                        <h2 className="text-lg font-extrabold">Conclusion</h2>
+                        <p className="text-sm">{data?.conclusion}</p>
+                    </div>
+                </div>
+
             </div>
         </div>
     )

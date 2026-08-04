a9b77e30db7add1b870cdaaec72fa09f288a0972
Ned Arctic <nedarctic@gmail.com>
2026-08-04 22:26:11 +0300

    hostname config for external images

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,10 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "pub-5cee8e6d1a574b6c84697dfdb9beba4a.r2.dev"
+      },
+      {
+        protocol: "https",
+        hostname: "pub-79ed953562964dbfa4ff96ef322c18ac.r2.dev"
       }
     ]
   }

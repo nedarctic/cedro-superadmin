8e968665e4392474535380e94f5cffa985d16ddf
Ned Arctic <nedarctic@gmail.com>
2026-08-04 10:22:27 +0300

    pass destination selec items as props

--- app/(dashboard)/tours/create-tour/page.tsx
+++ app/(dashboard)/tours/create-tour/page.tsx
@@ -1,13 +1,18 @@
-'use client'
-
 import { BreadCrumb } from "@/components/breadcrumb";
 import CreateTourForm from "@/components/create-tour-form";
+import { getDestinations } from "@/lib/helpers/destinations.helpers";
 
-export default function CreateTourPage() {
+export default async function CreateTourPage() {
     
     const crumbs = [
         {label: "Tours", link: '/tours'},
     ];
+
+    const url = `${process.env.NEST_API_URL}/destinations/all`;
+    const { data, success } = await getDestinations(url);
+    
+    const destinations = Array.isArray(data) ? data : data?.destinations;
+    const items = destinations?.map(destination => ({ label: destination.name, value: destination.id })) ?? [];
 
     return (
         <div className="flex flex-col py-6 ml-4 mr-6 gap-6">
@@ -15,7 +20,7 @@ export default function CreateTourPage() {
 
             <h1 className="font-bold text-xl">Tour Creation Form</h1>
 
-            <CreateTourForm />
+            <CreateTourForm items={items!} />
         </div>
     );
 }

3299d69cb1b7920a8d0e1e13b807dddf0f74dd37
Ned Arctic <nedarctic@gmail.com>
2026-08-05 14:15:38 +0300

    union return type destructure

--- app/(dashboard)/tours/[tourId]/edit-tour/page.tsx
+++ app/(dashboard)/tours/[tourId]/edit-tour/page.tsx
@@ -11,7 +11,10 @@ export default async function UpdateTourPage({ params }: {
 }) {
     const { tourId } = await params;
     const { success, data: tour, error } = await getTour(tourId);
-    const { data: destinations } = await getDestinations();
+    const url = `${process.env.NEST_API_URL}/destinations/all`;
+    const { data } = await getDestinations(url);
+
+    console.log('tour', tour);
 
     if (!success) {
         return notFound();
@@ -22,11 +25,13 @@ export default async function UpdateTourPage({ params }: {
         { label: 'Tour Details', link: `/tours/${tourId}` }
     ]
 
+    const destinations = Array.isArray(data) ? data : data?.destinations || [];
+
     return (
         <div className="flex flex-col py-6 ml-4 mr-6 gap-6 h-full">
             <BreadCrumb crumbs={crumbs} currentPage="Edit Tour" />
             <h1 className="font-bold text-xl">Edit Tour Form</h1>
-            <UpdateTourTestForm tour={tour!} destinations={destinations?.destinations!} />
+            <UpdateTourTestForm tour={tour!} destinations={destinations} />
         </div>
     )
 }

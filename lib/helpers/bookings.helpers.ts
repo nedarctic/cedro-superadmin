e6e54654f3b187a1d7b534faf66f12628206578c
Ned Arctic <nedarctic@gmail.com>
2026-08-06 12:40:17 +0300

    deconsole

--- lib/helpers/bookings.helpers.ts
+++ lib/helpers/bookings.helpers.ts
@@ -36,7 +36,6 @@ export async function getBookings(accessToken: string, options: {
         }
 
         const data = await res.json();
-        console.log('Bookings data at helper:', data);
 
         return {
             success: true,

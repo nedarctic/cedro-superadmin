ec9a0a882a0fdfb65385628beb3d60660f97815e
Ned Arctic <nedarctic@gmail.com>
2026-08-07 16:46:59 +0300

    field validation + payload form format update

--- components/create-blog-form.tsx
+++ components/create-blog-form.tsx
@@ -13,36 +13,26 @@ import { Form } from "./ui/form";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
 
+
 type StorySection = {
-    id: string;
-    section: string;
     subtitle: string;
     content: string;
-    image: File | null;
 };
 
-const initialBlogData = (section: number) => ({
-    id: crypto.randomUUID(),
-    section: `Section ${section}`,
+const initialBlogData = {
     subtitle: "",
     content: "",
-    image: null
-})
+}
 
 const storySectionSchema = z.object({
-    section: z.string().trim().min(1, "Section cannot be empty"),
     subtitle: z.string().trim().min(1, "Subtitle cannot be empty"),
     content: z.string().trim().min(1, "Content cannot be empty"),
-    image: z.instanceof(File)
-        .nullable()
-        .optional()
-        .refine(file => !file || file.size <= 5 * 1024 * 1024, { message: "Image size must be less than 5MB" })
-        .refine(file => !file || ['image/jpeg', 'image/png', 'image/gif'].includes(file.type), { message: "Only JPEG, PNG, and GIF images are allowed" })
 });
 
 const BlogCreationSchema = z.object({
     title: z.string().trim().min(1, "Title cannot be empty"),
     intro: z.string().trim().min(1, "Introduction cannot be empty"),
+    excerpt: z.string().trim().min(1, "Excerpt cannot be empty"),
     conclusion: z.string().trim().min(1, "Conclusion cannot be empty"),
     blogImage: z.instanceof(File)
         .refine(file => file.size > 0, { message: "Blog image is required" })
@@ -55,18 +45,17 @@ export function CreateBlogForm() {
 
     const router = useRouter();
     const [title, setTitle] = useState<string>("");
+    const [excerpt, setExcerpt] = useState<string>("");
     const [intro, setIntro] = useState<string>("");
     const [conclusion, setConclusion] = useState<string>("");
     const [blogImage, setBlogImage] = useState<File | null>(null);
-    const [storySections, setStorySections] = useState<StorySection[]>([initialBlogData(1)]);
+    const [storySections, setStorySections] = useState<StorySection[]>([initialBlogData]);
 
     const [errors, setErrors] = useState<any>({});
     const [loading, setLoading] = useState<boolean>(false);
 
-    const [generalError, setGeneralError] = useState<string | null>(null);
-
-    const addStorySection = (section: number) => {
-        return setStorySections(prev => [...prev, initialBlogData(section)])
+    const addStorySection = () => {
+        return setStorySections(prev => [...prev, initialBlogData])
     };
 
     const handleBlogSubmit = async (e: SubmitEvent<HTMLFormElement>) => {
@@ -74,56 +63,52 @@ export function CreateBlogForm() {
 
         try {
             setLoading(true);
-            const validationResult = BlogCreationSchema.safeParse({ title, intro, conclusion, blogImage, storySections });
+            const validationResult = BlogCreationSchema.safeParse({
+                title,
+                intro,
+                excerpt,
+                conclusion,
+                blogImage,
+                storySections
+            });
 
             if (!validationResult.success) {
                 setErrors(z.treeifyError(validationResult.error));
                 setLoading(false);
+                console.log("validation errors", z.treeifyError(validationResult.error));
                 return;
             }
 
             // blog data
-            const blogFormData = new FormData();
-            const sectionsWithImages: string[] = [];
+            const formData = new FormData();
 
-            blogFormData.append('blog',
+            formData.append('blog',
                 JSON.stringify({
                     title,
                     intro,
+                    excerpt,
+                    date: new Date().toLocaleDateString("en-KE", { day: "2-digit", month: "short", year: "numeric" }),
                     conclusion,
-                    sections: storySections.map(section => ({
-                        id: section.id,
-                        section: section.section,
+                    sections: storySections.map((section, index) => ({
+                        sectionNumber: index + 1,
                         subtitle: section.subtitle,
                         content: section.content
                     }))
                 })
             );
 
-            // section images
-            storySections.forEach((section, index) => {
-                if (section.image && section.image.size > 0) {
-                    sectionsWithImages.push(section.id);
-                    blogFormData.append('sectionImages', section.image as File);
-                }
-            })
-
             // blog image
-            blogImage && blogImage.size > 0 && blogFormData.append('blogImage', blogImage as File);
+            blogImage && blogImage.size > 0 && formData.append('image', blogImage as File);
 
-            // sections with images
-            blogFormData.append('sectionsWithImages', JSON.stringify({sectionsWithImages}));
-
-            const res = await fetch(`${process.env.NEXT_PUBLIC_FRONTEND_URL}/api/blogs`, {
+            const res = await fetch("/api/blogs", {
                 method: 'POST',
-                body: blogFormData
+                body: formData
             });
 
-            const { success, error, data } = await res.json();
+            const { data, success } = await res.json();
 
-            if (!success) {
-                toast.error(error || 'An unknown error occurred while creating the blog');
-                setGeneralError(error || 'An unknown error occurred');
+            if (!res.ok || !success) {
+                toast.error('An unknown error occurred while creating the blog');
                 setLoading(false);
                 return;
             }
@@ -142,7 +127,7 @@ export function CreateBlogForm() {
 
         } catch (error) {
             setLoading(false);
-            setGeneralError(error instanceof Error ? error.message : 'An unknown error occurred');
+            toast.error("Service temporarily unavailable. Please try again later.")
         }
     }
 
@@ -154,6 +139,11 @@ export function CreateBlogForm() {
                 {errors.properties?.title?.errors?.length && <ul className="list-disc pl-4">{errors.properties.title.errors.map((error: string, index: number) => (<li className="font-bold text-[12px] text-red-600" key={index}>{error}</li>))}</ul>}
             </Field>
             <Field>
+                <FieldLabel htmlFor="excerpt">Except</FieldLabel>
+                <Textarea id="excerpt" value={excerpt} onChange={(e) => setExcerpt(e.target.value)} required />
+                {errors.properties?.excerpt?.errors?.length && <ul className="list-disc pl-4">{errors.properties.excerpt.errors.map((error: string, index: number) => (<li className="font-bold text-[12px] text-red-600" key={index}>{error}</li>))}</ul>}
+            </Field>
+            <Field>
                 <FieldLabel htmlFor="intro">Introduction</FieldLabel>
                 <Textarea id="intro" value={intro} onChange={(e) => setIntro(e.target.value)} required />
                 {errors.properties?.intro?.errors?.length && <ul className="list-disc pl-4">{errors.properties.intro.errors.map((error: string, index: number) => (<li className="font-bold text-[12px] text-red-600" key={index}>{error}</li>))}</ul>}
@@ -163,7 +153,6 @@ export function CreateBlogForm() {
                 <Input accept="image/png, image/gif, image/jpeg" type="file" id="blogImage" onChange={(e) => setBlogImage(e.target.files ? e.target.files[0] : null)} required />
                 {errors.properties?.blogImage?.errors?.length && <ul className="list-disc pl-4">{errors.properties.blogImage.errors.map((error: string, index: number) => (<li className="font-bold text-[12px] text-red-600" key={index}>{error}</li>))}</ul>}
             </Field>
-            <FieldError>{generalError}</FieldError>
 
             <FieldGroup>
                 <FieldDescription className="font-semibold text-lg py-2">Story Sections</FieldDescription>
@@ -179,7 +168,7 @@ export function CreateBlogForm() {
                         </div>
                         <Field>
                             <FieldLabel htmlFor={`subtitle-${index}`}>Subtitle</FieldLabel>
-                            <Input required id={`subtitle-${index}`} value={section.subtitle} onChange={(e) => {
+                            <Input required id={`subtitle-${index}`} onChange={(e) => {
                                 const newSections = [...storySections];
                                 newSections[index].subtitle = e.target.value;
                                 setStorySections(newSections);
@@ -190,7 +179,7 @@ export function CreateBlogForm() {
 
                         <Field>
                             <FieldLabel htmlFor={`content-${index}`}>Content</FieldLabel>
-                            <Textarea required id={`content-${index}`} value={section.content} onChange={(e) => {
+                            <Textarea required id={`content-${index}`} onChange={(e) => {
                                 const newSections = [...storySections];
                                 newSections[index].content = e.target.value;
                                 setStorySections(newSections);
@@ -198,19 +187,10 @@ export function CreateBlogForm() {
                             {errors.properties?.storySections?.items?.[index]?.properties?.content?.errors?.length && <ul className="list-disc pl-4">{errors.properties.storySections.items[index].properties.content.errors.map((error: string, index: number) => (<li className="font-bold text-[12px] text-red-600" key={index}>{error}</li>))}</ul>}
                         </Field>
 
-                        <Field>
-                            <FieldLabel htmlFor={`image-${index}`}>Image</FieldLabel>
-                            <Input accept="image/png, image/gif, image/jpeg" type="file" id={`image-${index}`} onChange={(e) => {
-                                const newSections = [...storySections];
-                                newSections[index].image = e.target.files ? e.target.files[0] : null;
-                                setStorySections(newSections);
-                            }} />
-                            {errors.properties?.storySections?.items?.[index]?.properties?.image?.errors?.length && <ul className="list-disc pl-4">{errors.properties.storySections.items[index].properties.image.errors.map((error: string, index: number) => (<li className="font-bold text-[12px] text-red-600" key={index}>{error}</li>))}</ul>}
-                        </Field>
                     </div>
                 ))}
                 <div className="flex justify-start">
-                    <Button type="button" onClick={() => addStorySection(storySections.length + 1)}>
+                    <Button type="button" onClick={() => addStorySection()}>
                         <PlusIcon size={16} /> Add Section
                     </Button>
                 </div>
